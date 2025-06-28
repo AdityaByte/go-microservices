7fde3552156024732ea25b5ef9697a86b4949cf6
AdityaByte <adityapawar4914@gmail.com>
2025-06-28 19:56:15 +0530

    feat: added feature of order-service communicating with inventory-service for placing an order

--- order-service/internal/dto/order_request.go
+++ order-service/internal/dto/order_request.go
@@ -1,12 +1,14 @@
 package dto
 
+import "github.com/google/uuid"
+
 type OrderRequest struct {
 	OrderLineItemsDtoList []OrderLineItemsDto `json:"orderLineItemsDtoList"`
 }
 
 type OrderLineItemsDto struct {
-	Id       uint    `json:"id"`
-	SkuCode  string  `json:"skuCode"`
-	Price    float64 `json:"price"`
-	Quantity uint16  `json:"quantity"`
+	Id       uuid.UUID `json:"id"`
+	SkuCode  string    `json:"skuCode"`
+	Price    float64   `json:"price"`
+	Quantity uint16    `json:"quantity"`
 }
--- order-service/internal/usecase/order_usecase.go
+++ order-service/internal/usecase/order_usecase.go
@@ -1,6 +1,13 @@
 package usecase
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"sync"
+
 	"github.com/AdityaByte/order-service/internal/domain"
 	"github.com/AdityaByte/order-service/internal/dto"
 	"github.com/AdityaByte/order-service/internal/repository"
@@ -24,9 +31,77 @@ func PlaceOrder(orderRequest *dto.OrderRequest) error {
 	}
 
 	// Now we have to place the order.
-	err := repository.Save(&order)
-	if err != nil {
-		return err
+
+	var wg sync.WaitGroup
+	errorChan := make(chan error, 1)
+
+	wg.Add(1)
+	go func() {
+		req, err := http.NewRequest("GET", "http://localhost:8082/api/inventory", nil)
+		if err != nil {
+			errorChan <- err
+			wg.Done()
+			return
+		}
+
+		queryParams := url.Values{}
+		for _, value := range order.OrderLineItems {
+			queryParams.Add("skuCode", value.SkuCode)
+		}
+
+		req.URL.RawQuery = queryParams.Encode()
+
+		client := http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			errorChan <- err
+		}
+		defer resp.Body.Close()
+
+		var inventoryResponses []dto.InventoryResponse
+
+		if err := json.NewDecoder(resp.Body).Decode(&inventoryResponses); err != nil {
+			errorChan <- err
+			wg.Done()
+			return
+		}
+
+		fmt.Println(inventoryResponses)
+		log.Println("Decoded the data successfully...")
+
+		var isInStock bool
+		for _, inventoryResponse := range inventoryResponses {
+			if !inventoryResponse.IsInStock {
+				isInStock = false
+				break
+			} else {
+				isInStock = true
+			}
+		}
+
+		if isInStock {
+			errorChan <- nil
+			log.Println("Order placed successfully")
+		} else {
+			errorChan <- fmt.Errorf("Item is not in the stock")
+		}
+
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	select {
+	case err := <-errorChan:
+		if err != nil {
+			return err
+		} else {
+			repository.Save(&order)
+			return nil
+		}
+	default:
+		repository.Save(&order)
+		return nil
 	}
-	return nil
+
 }
